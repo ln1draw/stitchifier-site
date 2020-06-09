dc54bb0ca00595b702b0cdbd94742137a95a1d46
ellen wondra <ellenfromillinois@gmail.com>
2020-06-09 15:09:17 -0700

    initial work on layout

--- app/controllers/user_images_controller.rb
+++ app/controllers/user_images_controller.rb
@@ -4,26 +4,31 @@ class UserImagesController < ApplicationController
   # GET /user_images
   # GET /user_images.json
   def index
+    @title = "Welcome to Stitchify!"
     @user_images = UserImage.all
   end
 
   # GET /user_images/1
   # GET /user_images/1.json
   def show
+    @title = "Stitch"
   end
 
   # GET /user_images/new
   def new
+    @title = "Stitch Something!"
     @user_image = UserImage.new
   end
 
   # GET /user_images/1/edit
   def edit
+    @title = "Edit Your Stitch"
   end
 
   # POST /user_images
   # POST /user_images.json
   def create
+    @title = "Stitch Something!"
     @user_image = UserImage.new(user_image_params)
 
     respond_to do |format|
@@ -40,6 +45,7 @@ class UserImagesController < ApplicationController
   # PATCH/PUT /user_images/1
   # PATCH/PUT /user_images/1.json
   def update
+    @title = "Edit your Stitch"
     respond_to do |format|
       if @user_image.update(user_image_params)
         format.html { redirect_to @user_image, notice: 'User image was successfully updated.' }
@@ -54,6 +60,7 @@ class UserImagesController < ApplicationController
   # DELETE /user_images/1
   # DELETE /user_images/1.json
   def destroy
+    @title = "Destroy Your Stitch"
     @user_image.destroy
     respond_to do |format|
       format.html { redirect_to user_images_url, notice: 'User image was successfully destroyed.' }
