d0ebe263171092d43ec84702b7c57942fd7f9a1a
ellen wondra <ellenfromillinois@gmail.com>
2020-06-09 20:19:56 -0700

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_09_232659) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "user_images", force: :cascade do |t|
     t.string "name"
     t.string "stitch_pic"
